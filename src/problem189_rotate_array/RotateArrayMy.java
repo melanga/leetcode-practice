212ebef01d0c773312fe52b633e41d74dd423303
melanga <melangadev@icloud.com>
2025-08-04 22:04:06 +0530

    leetcode: 189: rotate array

--- src/problem189_rotate_array/RotateArrayMy.java
+++ src/problem189_rotate_array/RotateArrayMy.java
@@ -33,9 +33,22 @@ public class RotateArrayMy {
      * @param k the number of steps to rotate right
      */
     public void rotate(int[] nums, int k) {
-        //TODO
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
-    
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     /**
      * Test cases to verify your implementation
      */
